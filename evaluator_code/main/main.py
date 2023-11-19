f0aaf0efcc4bc31af0b60ae02c27ee6033acd576
Erick Santos <ericksresende@gmail.com>
2023-11-18 22:55:19 -0300

    atualizando aqui

--- evaluator_code/main/main.py
+++ evaluator_code/main/main.py
@@ -79,5 +79,6 @@ async def process_data(data: dict):
 
 if __name__ == '__main__':
     import uvicorn
+    port = int(os.environ.get("PORT", 5000))
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=port)
